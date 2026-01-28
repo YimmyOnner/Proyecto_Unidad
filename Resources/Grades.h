36aaf16531a631beb3c7ba167346fa8e3725e248
ProjectDiyer <diyertroya44@gmail.com>
2026-01-27 20:12:11 -0500

    Update Grades.h

--- Resources/Grades.h
+++ Resources/Grades.h
@@ -1,1 +1,23 @@
+#ifndef GRADES_H
+#define GRADES_H
 
+#include <stdio.h>
+
+// Estructura para representar un curso
+typedef struct {
+    int id;
+    char nombre[100];
+} Curso;
+
+typedef struct {
+    int id;
+    char nombre[100];
+    Curso cursos[10];
+    int numCursos;
+} Docente;
+
+void mostrarCursosDocente(Docente *docente);
+void cargarCursosDesdeCSV(Docente *docente);
+void guardarCursosEnCSV(Docente *docente);
+
+#endif // GRADES_H
