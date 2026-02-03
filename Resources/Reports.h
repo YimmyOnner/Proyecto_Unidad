48af2a721d0cbb2f8d492fa652a8107003b1215e
YimmyOnner <yimmyonner@gmail.com>
2026-02-03 17:07:42 -0500

    Update Reports.h

--- Resources/Reports.h
+++ Resources/Reports.h
@@ -1,7 +1,6 @@
 #ifndef REPORTS_H
 #define REPORTS_H
 
-// Ahora la función recibe el curso, mes, día y las listas de alumnos con su estado
-void escribirReporteFinal(char curso[], char mes[], char dia[], char nombres[10][30], char estados[10]);
+void escribirReporteFinal(char docente[], char curso[], char mes[], char dia[], char nombres[10][30], char estados[10]);
 
 #endif
