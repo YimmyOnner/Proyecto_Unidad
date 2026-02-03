69bf7789691976dd9de23a05fc37fb2100ca2884
YimmyOnner <yimmyonner@gmail.com>
2026-02-03 16:33:31 -0500

    Update asistencia.c

--- Resources/asistencia.c
+++ Resources/asistencia.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
-#include <string.h>
+#include "asistencia.h"
+#include "reports.h"
 
-int main() {
-    int n, i;
-    char nombres[50][50];
-    char asistencia[50];
+void registrarAsistencia(char curso[], int grupo)
+{
+    char estudiantes[3][10][30] = {
+        {"Ana", "Juan", "Maria", "Carlos", "Sofia", "Luis", "Paola", "Diego", "Elena", "Miguel"},
+        {"Jimmy", "Onner", "Elvis", "Hernan", "Torres", "Soto", "Medina", "Reyes", "Flores", "Guzman"},
+        {"Coronel", "Lopez", "Sebastian", "Mendoza", "Suarez", "Messi", "Adrian", "Vega", "Leonardo", "Aguirre"}};
 
-    printf("Ingrese el numero de estudiantes: ");
-    scanf("%d", &n);
+    char estados[10];
+    char mes[20], dia[20];
 
-    for (i = 0; i < n; i++) {
-        printf("\nEstudiante %d\n", i + 1);
+    // Se pide la fecha UNA sola vez aquí
+    printf("\n--- CONFIGURACION DE FECHA PARA EL REPORTE DIARIO ---\n");
+    printf("Ingrese Mes: ");
+    scanf("%s", mes);
+    printf("Ingrese Dia: ");
+    scanf("%s", dia);
 
-        printf("Nombre: ");
-        scanf("%s", nombres[i]);
-
-        printf("Asistencia (P = Presente, A = Ausente): ");
-        scanf(" %c", &asistencia[i]);
+    printf("\n--- TOMA DE ASISTENCIA ---\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Asistio %s? (P/A): ", estudiantes[grupo][i]);
+        scanf(" %c", &estados[i]);
     }
 
-    printf("\n--- LISTA DE ASISTENCIA ---\n");
-    for (i = 0; i < n; i++) {
-        printf("%s - ", nombres[i]);
+    // ENVIAR AL ARCHIVO SEPARADO
+    escribirReporteFinal(curso, mes, dia, estudiantes[grupo], estados);
 
-        if (asistencia[i] == 'P' || asistencia[i] == 'p') {
-            printf("Presente\n");
-        } else {
-            printf("Ausente\n");
-        }
-    }
-
-    return 0;
+    printf("\n[OK] Datos enviados al reporte por separado.\n");
 }
