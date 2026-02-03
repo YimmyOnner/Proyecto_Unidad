fd3150ed89508c011c70b67d0dba3317ba9a9f8f
YimmyOnner <yimmyonner@gmail.com>
2026-02-03 17:08:07 -0500

    Update Reports.c

--- Resources/Reports.c
+++ Resources/Reports.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "reports.h"
 
-void escribirReporteFinal(char curso[], char mes[], char dia[], char nombres[10][30], char estados[10]) {
+void escribirReporteFinal(char docente[], char curso[], char mes[], char dia[], char nombres[10][30], char estados[10]) {
     FILE *fp = fopen("reports.txt", "a");
     if (fp == NULL) return;
 
     fprintf(fp, "==========================================\n");
-    fprintf(fp, "REPORTE SEMANAL - CURSO: %s\n", curso);
-    fprintf(fp, "FECHA: %s de %s\n", dia, mes);
+    fprintf(fp, "DOCENTE: %s\n", docente);
+    fprintf(fp, "CURSO:   %s\n", curso);
+    fprintf(fp, "FECHA:   %s de %s\n", dia, mes);
     fprintf(fp, "------------------------------------------\n");
     fprintf(fp, "%-20s | %s\n", "ESTUDIANTE", "ASISTENCIA");
     fprintf(fp, "------------------------------------------\n");
@@ -15,7 +16,7 @@ void escribirReporteFinal(char curso[], char mes[], char dia[], char nombres[10]
     for (int i = 0; i < 10; i++) {
         fprintf(fp, "%-20s | %c\n", nombres[i], estados[i]);
     }
-
     fprintf(fp, "==========================================\n\n");
+
     fclose(fp);
 }
