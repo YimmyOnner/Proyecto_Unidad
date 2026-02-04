77a2a71935d1857062a828ca8bdc808a88ccf0b5
YimmyOnner <yimmyonner@gmail.com>
2026-02-03 21:23:34 -0500

    Delete Resources directory

--- Resources/asistencia.h
+++
@@ -1,7 +0,0 @@
-#ifndef ASISTENCIA_H
-#define ASISTENCIA_H
-
-// Definimos que recibe: Nombre Docente, Nombre Curso e ID de Grupo
-void registrarAsistencia(char docente[], char curso[], int grupo);
-
-#endif
--- Resources/reports.h
+++
@@ -1,6 +0,0 @@
-#ifndef REPORTS_H
-#define REPORTS_H
-
-void escribirReporteFinal(char docente[], char curso[], char mes[], char dia[], char nombres[10][30], char estados[10]);
-
-#endif
